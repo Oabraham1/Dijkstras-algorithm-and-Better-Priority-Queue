37634a8064efcff0305aa43108d60d80c9021d21
Oabraham1 <abrahamojima2018@gmail.com>
2020-09-24 17:02:06 -0400

    Initial commit

---
+++ BPQTests.cpp
@@ -0,0 +1,80 @@
+
+
+#include "Graph.h"
+#include "BetterPriorityQueue.h"
+#include <cassert>
+
+using namespace std;
+
+void ContainsTest() {
+	cout << "Testing Contains Helper Method..." << endl;
+	
+	Graph g = Graph();
+	BetterPriorityQueue q;
+	
+	GraphNode *a = g.AddNode('a');
+	DNode n;
+	n.node = a;
+	q.push(a);
+
+	assert(q.Contains(a) == 0);
+	
+
+	// more tests go here!
+	
+	
+	cout << "PASSED!" << endl;
+}
+
+
+
+void UpdateTest() {
+	cout << "Testing Update Helper Method..." << endl;
+	
+	Graph g = Graph();
+	BetterPriorityQueue q;
+	
+	g.AddNode('a');
+	g.AddNode('b');
+	g.AddNode('c');
+	g.AddNode('d');
+	GraphNode *e = g.AddNode('e');
+	GraphNode *f = g.AddNode('f');
+	vector<GraphNode*> nodes = g.GetNodes();
+	for(size_t i = 0; i < nodes.size(); i++){
+		DNode cur;
+		cur.pri = i;
+		cur.node = nodes.at(i);
+		//cout << "inserting cur.pri: " << cur.pri << "  node: " << cur.node->key << endl;
+		q.push(cur);
+		//cout << "size: " << q.size() << endl;
+	}
+	
+
+	//cout << "q before: " << queue_to_string(q) << endl;
+
+	DNode positive;
+	positive.node = f;
+	positive.pri = 0;
+	cout << "q before: " << q.ToString() << endl;
+	assert(q.Update(positive) == true);
+	cout << "q after: " << q.ToString() << endl;
+	assert(q.ToString() == "[(a: 0), (b: 1), (f: 0), (d: 3), (e: 4), (c: 2)]");
+	
+	
+	// more tests go here...
+	
+	
+	cout << "PASSED!" << endl;
+}
+
+int main(){
+	
+	ContainsTest();
+	UpdateTest();
+	
+	cout << "ALL TESTS PASSED!!" << endl;
+	
+	return 0;
+	
+}
---
+++ Graph.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+struct GraphNode {
+	char key;
+	int data; // Not necessary / used for Dijkstra algorithm
+};
+
+
+struct GraphEdge {
+	GraphNode *from;
+	GraphNode *to;	
+	unsigned int weight;
+};
+
+
+class Graph{
+	
+	public:
+		~Graph();
+	
+		GraphNode *AddNode(char key, int data = 0);
+		GraphEdge *AddEdge(GraphNode *gn1, GraphNode *gn2, unsigned int weight = 0);
+		
+		string NodesToString() const;
+		string ToString() const;
+
+		static string GraphNodeToString(const GraphNode *gn);
+		static string GraphEdgeToString(const GraphEdge *ge);
+		
+		
+		const vector<GraphEdge*>& GetEdges(const GraphNode *gn) const;
+		const vector<GraphNode*>& GetNodes() const;
+		const GraphNode* NodeAt(unsigned int idx) const;
+		size_t Size() const;
+		
+
+	private:
+		// Your code goes here!
+		vector<vector<GraphEdge>> adjacencyMatrix;
+		vector<GraphNode> nodeList;
+};
